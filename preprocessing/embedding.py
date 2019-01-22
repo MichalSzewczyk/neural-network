42a62214cb8be694801f019354cbbdaa502d31e3
mszewczyk <szewczykm94@gmail.com>
2019-01-22 20:29:36 +0100

    Added replacement of unknown words

--- preprocessing/embedding.py
+++ preprocessing/embedding.py
@@ -1,4 +1,5 @@
 import csv
+import random
 
 import pandas
 
@@ -8,17 +9,17 @@ class EmbeddingGenerator:
     def get_embedding(data_frame, file_name):
         glove = pandas.read_csv(file_name, sep=' ', header=None, quoting=csv.QUOTE_NONE,
                                 lineterminator='\n')
-        eos = pandas.np.random.uniform(low=-1.0, high=1.0, size=200)
-        bos = pandas.np.random.uniform(low=-1.0, high=1.0, size=200)
-        unk = pandas.np.random.uniform(low=-1.0, high=1.0, size=200)
+        eos = [random.uniform(-1, 1) for _ in range(201)]
+        bos = [random.uniform(-1, 1) for _ in range(201)]
+        unk = [random.uniform(-1, 1) for _ in range(201)]
         result_map = EmbeddingGenerator.get_as_map(glove)
         result_map['<eos>'] = eos
         result_map['<bos>'] = bos
         result_map['<unk>'] = unk
-        EmbeddingGenerator.count_and_log_all_wrods(data_frame)
+        EmbeddingGenerator.count_and_log_all_words(data_frame)
+        data_frame = EmbeddingGenerator.replace_unknown_words(data_frame, result_map)
+        EmbeddingGenerator.count_and_log_unknown_words(data_frame)
 
-        print('Supported words: ' + str(result_map))
-        data_frame['sentence1'].apply(lambda array: EmbeddingGenerator.replace_unknown(array))
         print('Embedding data: ' + str(data_frame))
         return data_frame
 
@@ -31,7 +32,7 @@ class EmbeddingGenerator:
         return result_map
 
     @staticmethod
-    def count_and_log_all_wrods(data_frame):
+    def count_and_log_all_words(data_frame):
         result_list = []
         for array in data_frame['sentence1']:
             for word in array:
@@ -42,5 +43,37 @@ class EmbeddingGenerator:
         print('Number of words: ' + str(len(result_list)))
 
     @staticmethod
-    def replace_unknown(array):
-        pass
+    def count_and_log_unknown_words(data_frame):
+        result_list = []
+        for array in data_frame['sentence1']:
+            for word in array:
+                if word is '<unk>':
+                    result_list.append(word)
+
+        for array in data_frame['sentence2']:
+            for word in array:
+                if word is '<unk>':
+                    result_list.append(word)
+
+        print('Number of unknown words: ' + str(len(result_list)))
+
+    @staticmethod
+    def replace_unknown_words(data_frame, result_map):
+        result_list = []
+        data_frame['sentence1'] = data_frame['sentence1'].apply(
+            lambda row: EmbeddingGenerator.replace_unknown(row, result_map, result_list))
+        data_frame['sentence2'] = data_frame['sentence2'].apply(
+            lambda row: EmbeddingGenerator.replace_unknown(row, result_map, result_list))
+        print('FOO: Number of words: ' + str(len(result_list)))
+        return data_frame
+
+    @staticmethod
+    def replace_unknown(array, result_map, replaced_words):
+        result_array = []
+        for word in array:
+            if word is '<bos>' or word is '<eos>' or word in result_map.keys():
+                result_array.append(word)
+            else:
+                result_array.append('<unk>')
+                replaced_words.append(word)
+        return result_array
