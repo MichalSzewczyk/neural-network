708479289c22113092ae5b5c8b64b98d3603accf
mszewczyk <szewczykm94@gmail.com>
2019-01-23 18:59:41 +0100

    Removed logging

--- preprocessing/statements_encoder.py
+++ preprocessing/statements_encoder.py
@@ -1,4 +1,3 @@
-import numpy as np
 import torch
 
 
@@ -16,18 +15,5 @@ class StatementsEncoder:
         joined_matrix_list = []
         for sentence_1_tensor, sentence_2_tensor in zip(sentence_1_tensor_list, sentence_2_tensor_list):
             joined_matrix_list.append(sentence_1_tensor + sentence_2_tensor)
-        print('FOO: ' + str(joined_matrix_list))
-        # sentence_tensor_1 = torch.from_numpy(data_frame['sentence1'].as_matrix())
-        # sentence_tensor_2 = torch.from_numpy(data_frame['sentence2'].as_matrix())
-        # print('Tensor:')
-        # print('Tensor1: ' + str(sentence_tensor_1))
-        # print('Tensor2: ' + str(sentence_tensor_2))
-        return data_frame
 
-    @staticmethod
-    def get_as_map(matrix):
-        for words in matrix:
-            for words2 in words:
-                for word in words2:
-                    print('Word: ' + str(type(word)))
-        return matrix
+        return data_frame
