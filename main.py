bec60445b5b9628ff3b518e330f8046704a17f3c
mszewczyk <szewczykm94@gmail.com>
2019-01-25 07:22:17 +0100

    Replaced objects creation with static methods invocation

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ from preprocessing.embedding_layer import EmbeddingLayerCreator
 from preprocessing.statements_encoder import StatementsEncoder
 from preprocessing.words_encoder import WordsEncoder
 
-data_loader = DataLoader()
-words_encoder = WordsEncoder()
 statements_encoder = StatementsEncoder()
-prepared_for_embedding = data_loader.load_file('project/dev.txt')
-words_to_vectors_mapping = words_encoder.prepare_embedding('data/test.txt')
+prepared_for_embedding = DataLoader.load_file('project/dev.txt')
+words_to_vectors_mapping = WordsEncoder.prepare_embedding('data/glove.6B.200d.txt')
 print('prepared for embedding: ' + str(prepared_for_embedding))
-prepared_for_embedding, words_to_vectors_mapping = data_loader.replace_and_remove_unknown_words(prepared_for_embedding,
-                                                                                                words_to_vectors_mapping)
+prepared_for_embedding, words_to_vectors_mapping = DataLoader.replace_and_remove_unknown_words(prepared_for_embedding,
+                                                                                               words_to_vectors_mapping)
 embedding = EmbeddingLayerCreator.create_layer(prepared_for_embedding, words_to_vectors_mapping)
