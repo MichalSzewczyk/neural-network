165caa39a72291387f289847c361b80c534388c2
mszewczyk <szewczykm94@gmail.com>
2019-01-26 19:52:47 +0100

    Added neural network module

---
+++ logic/NeuralNet.py
@@ -0,0 +1,22 @@
+from torch import nn
+
+
+class NeuralNet(nn.Module):
+    def __init__(self, embed_size, hidden_size):
+        super(NeuralNet, self).__init__()
+        self.embed_size = embed_size
+        self.hidden_size = hidden_size
+        self.target_size = target_size = 3
+        self.layers = nn.Sequential(
+            nn.Linear(embed_size, hidden_size),
+            self.relu,
+            self.dropout,
+            nn.Linear(hidden_size, target_size),
+            self.softmax
+        )
+        self.dropout = nn.Dropout(0.5)
+        self.relu = nn.ReLU(True)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, inputs):
+        return self.layers(inputs)
